0570949005b25eb46c414f3453de6a8bb9af99f1
RiefkyAdnan5 <2012500969@student.budiluhur.ac.id>
2023-04-13 09:23:21 +0700

    upload tugas 3 mysql

--- tugasmysql/tugasp3mysql.sql
+++ tugasmysql/tugasp3mysql.sql
@@ -21,8 +21,36 @@ SELECT * FROM produk WHERE min_stok > stok;
 //Tampilkan data pelanggan mulai dari yang paling muda
 SELECT * FROM pelanggan ORDER BY tgl_lahir DESC;
 //Tampilkan data pelanggan yang lahirnya di Jakarta dan gendernya perempuan
- SELECT * FROM pelanggan WHERE tmp_lahir='jakarta' AND jk='P';
+SELECT * FROM pelanggan WHERE tmp_lahir='jakarta' AND jk='P';
 //Tampilkan data pelanggan yang ID nya 2, 4 dan 6
 SELECT * FROM pelanggan WHERE id IN (2,4,6);
 //Tampilkan data produk yang harganya antara 500 ribu sampai 6 juta
 SELECT * FROM produk WHERE harga_jual >= 500000 AND harga_jual <= 6000000;
+
+//Soal 2.3
+//Tampilkan produk yang kode awalnya huruf K dan huruf M
+SELECT * FROM produk WHERE kode LIKE 'K%' OR kode LIKE 'M%';
+//Tampilkan produk yang kode awalnya bukan huruf M
+SELECT * FROM produk WHERE kode LIKE 'T%' OR kode LIKE 'K%';
+//Tampilkan produk-produk televisi
+SELECT * FROM produk WHERE nama LIKE 'TV%';
+//Tampilkan pelanggan mengandung huruf 'SA'
+SELECT * FROM pelanggan WHERE nama_pelanggan LIKE '%SA%';
+//Tampilkan pelanggan yang lahirnya bukan di Jakarta dan mengandung huruf ‘yo‘
+SELECT * FROM pelanggan WHERE tmp_lahir LIKE 'Kediri%' OR tmp_lahir LIKE 'Yogyakarta%' OR tmp_lahir LIKE 'Bandung%' OR nama_pelanggan LIKE '&yo&';
+//Tampilkan pelanggan yang karakter huruf ke – 2 nya adalah A
+SELECT * FROM pelanggan WHERE nama_pelanggan LIKE '%A%';
+
+//Soal 2.4
+//Tampilkan 2 data produk termahal
+SELECT * FROM produk ORDER BY harga_beli DESC LIMIT 2;
+//Tampilkan produk yang paling murah
+SELECT * FROM produk ORDER BY harga_beli;
+//Tampilkan produk yang stoknya paling banyak
+SELECT * FROM produk ORDER BY stok DESC LIMIT 2;
+//Tampilkan dua produk yang stoknya paling sedikit
+SELECT * FROM produk ORDER BY stok LIMIT 2;
+//Tampilkan pelanggan yang paling muda
+SELECT * FROM pelanggan ORDER BY tgl_lahir DESC LIMIT 1;
+//Tampilkan pelanggan yang paling tua
+SELECT * FROM pelanggan ORDER BY tgl_lahir LIMIT 1;
